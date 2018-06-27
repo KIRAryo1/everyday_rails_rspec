0c5a30a37a49411b5f182762eb4c5463b6689f27
Ryoichi Kira <r.kira@grooves.co.jp>
2018-06-27 10:58:04 +0900

    test unloggined user

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -21,4 +21,34 @@ RSpec.feature "Projects", type: :feature do
       expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
   end
+
+  scenario "guest adds a project" do
+    visit projects_path
+    save_and_open_page
+    click_link "New Project"
+  end
 end
+
+# scenario "works with all kinds of HTML elements" do
+#   visit "/fake/page"
+#   click_on "A link or button label"
+#   check "A checkbox label"
+#   uncheck "A checkbox label"
+#   choose "A radio button label"
+#   select "An option", from: "A select menu"
+#   attach_file "A file upload label", "/some/file/in/my/test/suite.gif"
+#
+#   expect(page).to have_css "h2#subheading"
+#   expect(page).to have_selector "ul li"
+#   expect(page).to have_current_path "project/new"
+# end
+#
+# within "#rails" do
+#   click_link "click here!"
+# end
+#
+# language = find_field("Programming language").value
+# expect(language).to eq "Ruby"
+#
+# find("#fine_print").find("#disclaimer").click
+# find_button("Publish").click
